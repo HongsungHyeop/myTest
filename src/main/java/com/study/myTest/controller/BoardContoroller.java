d0d6d08198b41ec3f7926ca67ae2e42951ad9d67
Hong <tjdguqdl@gmail.com>
2020-12-01 15:59:34 +0900

    2020/12/01

--- src/main/java/com/study/myTest/controller/BoardContoroller.java
+++ src/main/java/com/study/myTest/controller/BoardContoroller.java
@@ -4,13 +4,15 @@ import com.study.myTest.modal.Board;
 import com.study.myTest.repository.BoardRepository;
 import com.study.myTest.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -23,9 +25,16 @@ public class BoardContoroller {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
+    public String list(Model model,@PageableDefault(size = 2) Pageable pageable,
+                      @RequestParam(required = false, defaultValue = "") String searchText) {
+//        Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
 
-        List<Board> boards = boardRepository.findAll();
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
